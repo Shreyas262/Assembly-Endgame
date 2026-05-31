d3542dabc9ca47291fecbc516f1aebf64ff53f8f
Shreyas262 <shreyasmahajan2602@gmail.com>
2026-05-31 17:02:59 +0530

    Added CSS for keyboard and word guesses

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import { useState, useEffect, useRef } from 'react'
 import Header from './components/HeaderContent.jsx'
 import StatusSection from './components/StatusSection.jsx'
 import LanguageList from './components/LanguageList.jsx'
-import Keyboard from './components/Keyboard.jsx'
+import { clsx } from 'clsx'
 import './App.css'
 
 function AssemblyEndgame() {
+  const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
   const [currentWord, setCurrentWord] = useState('react')
+  const [guessedLetters, setGuessedLetters] = useState([])
+
   const letterComponents = currentWord.split('').map((letter, index) => {
     return (
       <span key={index} className='letter-box'>{letter.toUpperCase()}</span>
     )
   })
+  
   return (
     <main>
       <Header />
@@ -20,7 +25,27 @@ function AssemblyEndgame() {
       <section className="current-word-container">
         {letterComponents}
       </section>
-      <Keyboard />
+      <section className="keyboard-container">
+        {alphabets.map((alpha) =>
+        {
+          const isGuessed = guessedLetters.includes(alpha)
+          const isCorrect = isGuessed && currentWord.includes(alpha)
+          const isWrong = isGuessed && !currentWord.includes(alpha)
+          const className = clsx(
+            {
+              correct: isCorrect, wrong: isWrong
+            })
+          console.log(className)
+
+          return (<button key={alpha} className={className} onClick={
+            () => setGuessedLetters((prev) =>
+              prev.includes(alpha) ? prev : [...prev, alpha])
+            }
+          >{alpha.toLocaleUpperCase()}</button>)
+          }
+        )}
+
+        </section>
       <button className="new-game-btn">New Game</button>
     </main>
   )
--- src/components/Keyboard.jsx
+++
@@ -1,9 +0,0 @@
-export default function Keyboard() {
-    const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('')
-    return (
-        <section className="keyboard-container">
-            {alphabets.map((alpha) => <button key={alpha} className="key">{alpha.toLocaleUpperCase()}</button>)}
-
-        </section>
-    )
-}
